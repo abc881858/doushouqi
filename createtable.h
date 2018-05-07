622c9f8656e3f1873ba791d5a497f3ebbf8b7cb9
吴冬亮 <abc8333@gmail.com>
2018-05-07 09:18:33 +0800

    Update createtable.h

--- createtable.h
+++ createtable.h
@@ -1,6 +1,8 @@
 #ifndef CREATETABLE_H
 #define CREATETABLE_H
 
+// this file is for creating the table
+
 #include <QFrame>
 #include <QLabel>
 
@@ -9,17 +11,19 @@ class CreateTable : public QFrame
     Q_OBJECT
 public:
     explicit CreateTable(QWidget *parent = nullptr);
+
 protected:
     void paintEvent(QPaintEvent *event);
     void mousePressEvent(QMouseEvent *event);
     void mouseMoveEvent(QMouseEvent *event);
+
 private:
     QLabel* allLabels[8][8];
     int labelRow;
     int labelColumn;
+
 signals:
     void showTable(int,int);
-public slots:
 };
 
 #endif // CREATETABLE_H
