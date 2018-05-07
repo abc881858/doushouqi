74a3006f8c2905a6265fac535ef88839995125a5
吴冬亮 <abc8333@gmail.com>
2018-05-07 09:19:49 +0800

    Update leftwidget.h

--- leftwidget.h
+++ leftwidget.h
@@ -1,20 +1,23 @@
 #ifndef LEFTWIDGET_H
 #define LEFTWIDGET_H
 
-#include <QWidget>
+// one ChartView has one LeftWidget
+// to show different checkbox open or close
+
 #include <QFormLayout>
+#include <QList>
+#include <QWidget>
 
 class LeftWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit LeftWidget(QWidget *parent = nullptr);
+    QList<int> list;
 
 signals:
     void openCheck(int);
     void closeCheck(int);
-
-public slots:
 };
 
 #endif // LEFTWIDGET_H
