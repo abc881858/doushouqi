ecee55b044aeeeb7493ca4562fe3b1264b4cd387
吴冬亮 <abc8333@gmail.com>
2018-05-07 09:23:44 +0800

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,8 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QStackedWidget>
+#include <QDialog>
 #include <QGridLayout>
+#include <QImage>
+#include <QLabel>
+#include <QPixmap>
+#include <QStackedWidget>
+#include <QTime>
+#include <QWidget>
+#include <QtAV/AVPlayer.h>
+#include <QtAV/Statistics.h>
+#include <QtAVWidgets/VideoPreviewWidget.h>
+#include "chartwidget.h"
 
 class MainWindow : public QMainWindow
 {
@@ -13,9 +23,18 @@ public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-    QStackedWidget *left;
+    QStackedWidget *leftStack;
     QWidget *right;
+    QWidget *left;
     QGridLayout *gridLayout;
+    QVBoxLayout *vboxLayout;
+    QLabel *pictureLabel;
+    QImage m_image;
+    QDialog *dialog;
+
+private:
+    QtAV::VideoPreviewWidget *m_preview;
+    int m_duration;
 
 };
 
