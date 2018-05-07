008fa47022b668227f2e4625a94f6ddb0daed9dd
吴冬亮 <abc8333@gmail.com>
2018-05-07 09:22:53 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QTranslator>
-
+ 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
     QTranslator translator;
     translator.load(QString("zh_cn.qm"));
     a.installTranslator(&translator);
-
+ 
     MainWindow w;
     w.showMaximized();
-
+ 
     return a.exec();
 }
